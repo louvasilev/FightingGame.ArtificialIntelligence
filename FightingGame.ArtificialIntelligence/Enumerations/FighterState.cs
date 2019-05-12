3fdd6a94715afa791d0f3e7521f7a494e8e173ba
louvasilev <lougv22@yahoo.com>
2019-05-12 12:29:20 -0500

    Add files via upload

---
+++ FightingGame.ArtificialIntelligence/Enumerations/FighterState.cs
@@ -0,0 +1,25 @@
+﻿
+namespace AI.FiniteStateMachine.Enumerations
+{
+    /// Copyright © 2015-2019 Lou Vasilev - All rights reserved.
+    ///
+    /// <summary>
+    /// An enumeration for all possible states the Artificial
+    /// Intelligence (AI) controlled fighter can be in.
+    /// </summary>
+    /// 
+    /// <author>
+    /// Lou Vasilev
+    /// </author>
+    /// <date>
+    /// 11-21-2015
+    /// </date>
+    public enum FighterState
+    {
+	    None = 0,
+	    Observing = 1,
+	    Strategizing = 2,
+	    Deciding = 3,
+	    Performing = 4
+    }
+}
